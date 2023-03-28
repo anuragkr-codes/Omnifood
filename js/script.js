5cd8ba56cb87243e78c340004cd538236968c7a8
Anurag <tech.anuragkr@gmail.com>
2023-03-28 20:16:22 +0530

    feat: make the mobile nav work
    - use js to toggle the classList of header
    - fix the focus of toggle button using z-index property

--- js/script.js
+++ js/script.js
@@ -12,10 +12,22 @@ console.log(h1);
 //   h1.style.padding = "5rem";
 // });
 
+///////////////////////////////////////////////////////////
+//set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+//Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
